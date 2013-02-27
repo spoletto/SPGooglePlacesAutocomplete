3c8d53b3cdb8f51f8cb9b13a9a9f0f53dc68c14b
Ignacio Romero <iromero@dzen.cl>
2013-02-27 13:50:01 -0300

    Added a warning to the APIKey.h

--- Source/APIKey.h
+++ Source/APIKey.h
@@ -1,7 +1,7 @@
 
-// To use GooglePlacesAutocomplete, please register an APIKey for your application
-// and set it here.
-#define kGoogleAPIKey @"YOUR_API_KEY"
+#warning To use GooglePlacesAutocomplete, please register an APIKey for your application and set it here.
+
+#define kGoogleAPIKey @""
 
 // DO NOT CHECK IN CHANGES TO THIS FILE.
 // Your API Key is private. To tell git to ignore local changes to this file,
