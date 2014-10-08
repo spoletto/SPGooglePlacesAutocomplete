d5dec5d2a7556fc16e7d013de192f60570d1cba9
Daryll Herberger <daryllh@vertigo.com>
2014-10-08 14:08:36 -0700

    Updating SPGooglePlacesAutocompletePlace logic to rely on placeIdentifier instead of the less-reliable geocoder for retrieving additional place detail

--- SPGooglePlacesAutocomplete/SPGooglePlacesAutocompletePlace.m
+++ SPGooglePlacesAutocomplete/SPGooglePlacesAutocompletePlace.m
@@ -43,46 +43,19 @@
     return geocoder;
 }
 
-- (void)resolveEstablishmentPlaceToPlacemark:(SPGooglePlacesPlacemarkResultBlock)block {
+- (void)resolveReferenceIdentifierToPlacemark:(SPGooglePlacesPlacemarkResultBlock)block {
     SPGooglePlacesPlaceDetailQuery *query = [SPGooglePlacesPlaceDetailQuery query];
     query.placeIdentifier = self.identifier;
     [query fetchPlaceDetail:^(NSDictionary *placeDictionary, NSError *error) {
         if (error) {
             block(nil, nil, error);
         } else {
-            if ([placeDictionary objectForKey:@"geometry"]) {
-                CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(360.0f, 360.0f);
-                
-                NSDictionary *location = [[placeDictionary objectForKey:@"geometry"] objectForKey:@"location"];
-                
-                if ([location objectForKey:@"lat"] && [location objectForKey:@"lng"]) {
-                    CGFloat latitude = 360.0f;
-                    CGFloat longitude = 360.0f;
-                    
-                    if ([location objectForKey:@"lat"]) {
-                        latitude = [[location objectForKey:@"lat"] floatValue];
-                    }
-                    
-                    if ([location objectForKey:@"lng"]) {
-                        longitude = [[location objectForKey:@"lng"] floatValue];
-                    }
-                    
-                    coordinate = CLLocationCoordinate2DMake(latitude, longitude);
-                }
-                
-                if (CLLocationCoordinate2DIsValid(coordinate)) {
-                    MKPlacemark *placemark = [[MKPlacemark alloc] initWithCoordinate:coordinate addressDictionary:nil];
-                    block(placemark, self.name, error);
-                    return;
-                }
-            }
-            
             NSString *addressString = [placeDictionary objectForKey:@"formatted_address"];
-            [geocoder geocodeAddressString:addressString completionHandler:^(NSArray *placemarks, NSError *error) {
+            [[self geocoder] geocodeAddressString:addressString completionHandler:^(NSArray *placemarks, NSError *error) {
                 if (error) {
-                    SPPresentAlertViewWithErrorAndTitle(error, @"Could not map selected Place");
+                    block(nil, nil, error);
                 } else {
-                    CLPlacemark *placemark = [placemarks onlyObject];
+                    CLPlacemark *placemark = [placemarks firstObject];
                     block(placemark, self.name, error);
                 }
             }];
@@ -95,18 +68,20 @@
         if (error) {
             block(nil, nil, error);
         } else {
-            CLPlacemark *placemark = [placemarks onlyObject];
+            CLPlacemark *placemark = [placemarks firstObject];
             block(placemark, self.name, error);
         }
     }];
 }
 
 - (void)resolveToPlacemark:(SPGooglePlacesPlacemarkResultBlock)block {
-    if (type == SPPlaceTypeGeocode) {
+    if (self.identifier.length > 0) {
+        // If there is already a Google Place API reference number, there should
+        // not be a need to attempt to geocode using the (inexact) address.
+        [self resolveReferenceIdentifierToPlacemark:block];
+    } else {
         // Geocode places already have their address stored in the 'name' field.
         [self resolveGecodePlaceToPlacemark:block];
-    } else {
-        [self resolveEstablishmentPlaceToPlacemark:block];
     }
 }
 
